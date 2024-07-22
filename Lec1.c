325d3fe1931148676978d9fe77abfe535950da25
Rydham <rydhampreetsingh.gindra@gmail.com>
2024-07-22 11:16:33 -0400

    Lect1 Done

--- Lec1.c
+++ Lec1.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 #include <assert.h>
 void exch(int arr[], int i, int j)
 {
@@ -28,6 +31,29 @@ void print_array(int arr[], int n){
     
 }
 
+long long time_elapsed(int n)
+{
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    srand(time(NULL));
+
+    for(int i=0;i<n;i++) arr[i] = rand();
+
+    struct timeval before;
+    gettimeofday(&before, NULL);
+    long long before_millis = before.tv_sec*1000LL + before.tv_usec/1000;
+
+    selection_sort(arr, n);
+
+     struct timeval after;
+    gettimeofday(&after, NULL);
+    long long after_millis = after.tv_sec*1000LL + after.tv_usec/1000;
+
+    return after_millis-before_millis;
+
+
+}
+
 void test_sample_input(){
     int arr[] = {5, 3, 1, 9, 7};
     int expected_output[] = {1, 3, 5, 7, 9};
@@ -42,6 +68,24 @@ void test_sample_input(){
 
 int main()
 {
-    test_sample_input();
+
+
+    // struct timeval before;
+    // gettimeofday(&before, NULL);
+    // long long before_millis = before.tv_sec*1000LL + before.tv_usec/1000;
+    // printf("Hello");
+
+    // struct timeval after;
+    // gettimeofday(&after, NULL);
+    // long long after_millis = after.tv_sec*1000LL + after.tv_usec/1000;
+
+    // printf("Printf ran in : %llu millis seconds", after_millis-before_millis);
+    // test_sample_input();
+
+
+    long long t1 = time_elapsed(32000);
+    long long t2 = time_elapsed(64000);
+
+    printf("%lf", t2*1.0/t1);
     return 0;
 }
--- Lec1_2.c
+++ Lec1_2.c
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include<limits.h>
 
 
 int main()
 {
+
+    printf("%d\n", INT_MAX);
     // Measuring Program Runtime
     struct timeval before;
     gettimeofday(&before, NULL);
     long long before_millis = before.tv_sec*1000LL + before.tv_usec/1000;
-    printf("Hello");
+    // printf("Hello");
 
     struct timeval after;
     gettimeofday(&after, NULL);
     long long after_millis = after.tv_sec*1000LL + after.tv_usec/1000;
 
-    printf("Printf ran in : %llu millis seconds", after_millis-before_millis);
+    // printf("Printf ran in : %llu millis seconds", after_millis-before_millis);
 
 
     //Random Numbers
