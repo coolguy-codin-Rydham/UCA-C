27f84752bf265f60e9fe4e73837c0344557b8a14
vagrant <vagrant@kali.>
2024-07-29 11:25:44 -0400

    Stack Implementation using LL and Array

---
+++ stack.h
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+#include <stdbool.h>
+
+void push(int item);
+int pop();
+bool isEmpty();
+int size();
+
+
+
+
+// int main(){    
+//     int a = 10;
+
+//     printf("%d", a);
+//     return 0;
+// }
