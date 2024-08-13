98fbdffeb8d40bc411e214ade0db6e793e24bfa0
Rydhampreet Singh Gindra <rydhampreetsingh.gindra@gmail.com>
2024-08-13 18:42:26 +0530

    Queue using LL

--- UCA_C_KG_SIR/queueUsingLL.c
+++ UCA_C_KG_SIR/queueUsingLL.c
@@ -1,18 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include  <limits.h>
+#include <assert.h>
+#include <limits.h>
 #include "queue.h"
 
-typedef struct Node{
+typedef struct Node
+{
     int data;
     struct Node *next;
-}Node;
+} Node;
 
+typedef struct Queue
+{
+    Node *front;
+    Node *rear;
+    int queueSize;
+} Queue;
 
+Node *front = NULL;
+Node *rear = NULL;
+int queueSize = 0;
 
+Node *newNode(int item)
+{
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->data = item;
+    temp->next = NULL;
+    return temp;
+};
 
-int main(){
-    
+void enqueue(int item)
+{
+    Node *temp = newNode(item);
+    if (front == NULL)
+    {
+        front = temp;
+        rear = temp;
+    }
+    else
+    {
+        rear->next = temp;
+        rear = temp;
+    }
+    queueSize++;
+}
+
+int dequeue()
+{
+    if (front == NULL)
+    {
+        return INT_MIN;
+    }
+    Node *temp = front;
+    int item = temp->data;
+    front = front->next;
+    if (front == NULL)
+    {
+        rear = NULL;
+    }
+
+    free(temp);
+    queueSize--;
+    return item;
+}
+
+bool isEmpty()
+{
+    return (front == NULL);
+}
+
+int size()
+{
+    return queueSize;
+}
+
+void test()
+{
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+
+    assert(dequeue() == 1);
+    assert(size()==2);
+    assert(isEmpty() == false);
+
+    assert(dequeue() == 2);
+    assert(size()==1);
+    assert(isEmpty() == false);
+
+    assert(dequeue()==3);
+    assert(size()==0);
+    assert(isEmpty() == true);
+}
+
+int main()
+{
+    test();
+    printf("Passed All Test Cases");
 
     return 0;
 }
